fb7e88a1bc5402ddf6788065dfdbbcc8dfbd42c1
SxhkdUser <programLake@protonmail.com>
2026-03-25 22:02:33 +0800

    Day 6.1

---
+++ SatelliteDataLibrary/Algorithms/SearchingAlgorithms.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace SatelliteDataLibrary
+{
+    public class SearchingAlgorithm<T> where T : IComparable<T>, IConvertible
+    {
+        private Func<LinkedList<T>, T, int, int, int> _search;
+        
+        public enum SearchType
+        {
+            Interative,
+            Recursive
+        }
+        private int BinarySearchRecursive(LinkedList<T> data, T searchValue, int minIndex, int maxIndex)
+        {
+            if (data == null || data.First == null) return -1;
+            if (minIndex > maxIndex)
+            {    
+                if (minIndex >= data.Count) minIndex = data.Count - 1;
+                var nearest = data.First;
+                for (int i = 0; i < minIndex; i++)
+                    nearest = nearest.Next;
+                return nearest.Value.ToInt32(null);
+            }
+
+            int midIndex = (minIndex + maxIndex) / 2;
+
+            var current = data.First;
+            for (int i = 0; i < midIndex; i++)
+            {
+                current = current.Next;
+            }
+            if (current.Value.CompareTo(searchValue) == 0)
+            {
+                return current.Value.ToInt32(null);
+            }               
+            else if (current.Value.CompareTo(searchValue) < 0)
+            {
+                return BinarySearchRecursive(data, searchValue, midIndex + 1, maxIndex);
+            }               
+            else
+            {
+                return BinarySearchRecursive(data, searchValue, minIndex, midIndex - 1);
+            }            
+        }
+        private int BinarySearchInterative(LinkedList<T> data, T UsrInput, int minIndex, int maxIndex)
+        {
+            if (data == null || data.First == null ) return -1;
+            if (minIndex > maxIndex)
+            {
+                if (minIndex >= data.Count) minIndex = data.Count - 1;
+                var nearest = data.First;
+                for (int i = 0; i < minIndex; i++)
+                    nearest = nearest.Next;
+                return nearest.Value.ToInt32(null);
+            }
+
+            while (minIndex <= maxIndex - 1)
+            {
+                int midIndex = (minIndex + maxIndex) / 2;
+
+                var currentVal = data.First;
+                for (int i = 0; i < midIndex; i++)
+                {
+                    currentVal = currentVal.Next;
+                }
+                if (currentVal.Value.CompareTo(UsrInput) == 0)
+                {
+                    return currentVal.Value.ToInt32(null);
+                }
+                else if (currentVal.Value.CompareTo(UsrInput) < 0)
+                {
+                    maxIndex = midIndex + 1;
+                }
+                else
+                {
+                    minIndex = midIndex + 1;
+                }
+
+            }
+            return -1;
+        }
+        public void SelectSearch(SearchType type)
+        {
+            _search = type switch
+            {
+                SearchType.Interative => BinarySearchInterative,
+                SearchType.Recursive => BinarySearchRecursive,
+                _ => throw new ArgumentException()
+            };
+        }
+        public int DataSearch(LinkedList<T> data, T UsrInput, int min, int max)
+        {
+           return _search(data, UsrInput, min, max);
+        }
+    }
+}
---
+++ SatelliteDataLibrary/Algorithms/SortingAlgorithms.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SatelliteDataLibrary
+{
+    public class SortingAlgorithms<T> where T : IComparable<T>
+    {
+        private Func<LinkedList<T>, LinkedList<T>> _sort;
+        public enum SortType
+        {
+            Selection,
+            Insertion
+        }
+        private LinkedList<T> SelectionSort(LinkedList<T> data)
+        {
+            var outer = data.First;
+            while (outer != null)
+            {
+                var min = outer;
+                var inner = outer.Next;
+                while (inner != null)
+                {
+                    if (inner.Value.CompareTo(min.Value) < 0)
+                    {
+                        min = inner;
+                    }
+                    inner = inner.Next;
+                }
+                if (!Object.ReferenceEquals(min, outer))
+                {
+                    T temp = outer.Value;
+                    outer.Value = min.Value;
+                    min.Value = temp;
+                }
+                outer = outer.Next;
+            }
+            return data;
+
+        }
+        private LinkedList<T> InsetionSort(LinkedList<T> data)
+        {
+            if (data.First == null)
+            {
+                return null;
+            }
+
+            var current = data.First.Next;
+
+            while (current != null)
+            {
+                var next = current.Next; 
+
+                var search = current.Previous;
+
+                while (search != null && search.Value.CompareTo(current.Value) > 0)
+                {
+                    search = search.Previous;
+                }
+
+                data.Remove(current);
+
+                if (search == null)
+                {
+                    data.AddFirst(current);
+                }
+                else
+                {
+
+                    data.AddAfter(search, current);
+                }
+
+                current = next;
+            }
+            return data;
+        }
+        public void SelectSort(SortType type)
+        {
+            _sort = type switch
+            {
+                SortType.Selection => SelectionSort,
+                SortType.Insertion => InsetionSort,
+                _ => throw new ArgumentException()
+            };
+        }
+        public LinkedList<T> DataSearch(LinkedList<T> data)
+        {
+            return _sort(data);
+        }
+    }
+}
