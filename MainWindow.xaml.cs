2807cd12c3db34cd7f1d76e622953624e7c7d6eb
sxhkdtafe <30105350@tafe.wa.edu.au>
2026-03-27 10:32:08 +0800

    Day 8.1 this broke

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using SatelliteDataLibrary.Controllers;
 
 namespace SatelliteDataProcessing
 {
@@ -16,9 +17,21 @@ namespace SatelliteDataProcessing
     /// </summary>
     public partial class MainWindow : Window
     {
+        DisplayFuncController? controller;
         public MainWindow()
         {
+            
             InitializeComponent();
+
+
+            try
+            {
+                controller = new DisplayFuncController();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
         }
         private void BinRCA1Btn_Click(object sender, RoutedEventArgs e)
         {
@@ -51,6 +64,36 @@ namespace SatelliteDataProcessing
         private void SortIsrtB2Btn_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+        private void LoadDataBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Display(AllDtaListBox, SnsrA1LstView, SnsrB1LstView, MuTxt, SigmaTxt); 
+        }
+        private void Display(ListBox ListBox, ListView ListA, ListView ListB, TextBox mu, TextBox sigma)
+        {
+
+            if (!int.TryParse(mu.Text, out int muValue) || !int.TryParse(sigma.Text, out int sigmaValue))
+            {
+                MessageBox.Show("Invalid input");
+                return;
+            }
+            if (ListBox == null) return;
+            if (ListA == null) return;
+            if (ListB == null) return;
+            ListBox.Items.Clear();
+            ListA.Items.Clear();
+            ListB.Items.Clear();
+            var lists = controller.DataHandler(sigmaValue, muValue);
+            foreach (var sensor in lists.A)
+            {
+                ListBox.Items.Add($"A: {sensor}");
+                ListA.Items.Add(sensor);
+            }
+            foreach (var sensor in lists.B)
+            {
+                ListBox.Items.Add($"B: {sensor}");
+                ListB.Items.Add(sensor);
+            }
         }
     }
     
---
+++ SatelliteDataLibrary/Controllers/DisplayFuncController.cs
@@ -0,0 +1,33 @@
+﻿using SatelliteDataLibrary.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SatelliteDataLibrary.Controllers
+{
+    public class DisplayFuncController
+    {
+        private DataManagement Basic = new DataManagement();
+
+        public (List<string> A, List<string> B) DataHandler(int sig, int mu)
+        {
+            List<string> listA = new List<string>();
+            List<string> listB = new List<string>();
+            Basic.LoadDataToList(mu, sig, DataManagement.Sensors.SensorA);
+            Basic.LoadDataToList(mu, sig, DataManagement.Sensors.SensorB);
+            var sensorAData = Basic.ShowAllSensorData(DataManagement.Sensors.SensorA);
+            var sensorBData = Basic.ShowAllSensorData(DataManagement.Sensors.SensorB);
+            foreach( var sensor in sensorAData)
+            {
+                listA.Add(sensor.ToString());
+            }
+            foreach (var sensor in sensorBData)
+            {
+                listB.Add(sensor.ToString());
+            }
+            return (listA,listB);
+        }
+    }
+}
